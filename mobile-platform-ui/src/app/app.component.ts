e066ac53ab7e2791c7a586ec6ede2d466b460dad
Amruta Jegarkal <amruta@simpragma.com>
2018-03-16 11:53:57 +0530

    Adds home page with drag and drop events for two widgets

--- mobile-platform-ui/src/app/app.component.ts
+++ mobile-platform-ui/src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+	title = 'Mobile platform UI';
+
+	onDragStart(event, data) {
+		event.dataTransfer.setData('data', data);
+	}
+	
+	onDrop(event, data) {
+		let dataTransfer = event.dataTransfer.getData('data');
+		console.log("Dropped", event)
+		event.preventDefault();
+	}
+
+	allowDrop(event) {
+		event.preventDefault();
+	}
+
 }
+
+
