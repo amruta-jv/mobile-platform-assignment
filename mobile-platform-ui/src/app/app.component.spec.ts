f3e3c3d87bfa4f704a8d0f448e647dd8cbb2b067
Amruta Jegarkal <amruta@simpragma.com>
2018-03-19 15:11:39 +0530

    Adds tests to app component

--- mobile-platform-ui/src/app/app.component.spec.ts
+++ mobile-platform-ui/src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing'
 
 import { AppComponent } from './app.component';
 
@@ -8,6 +9,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [ RouterTestingModule ]
     }).compileComponents();
   }));
 
@@ -17,16 +19,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Mahindra Comviva Mobile Platform UI'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.title).toEqual('Mahindra Comviva Mobile Platform UI');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a span tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('span').textContent).toContain('Mahindra Comviva Mobile Platform UI');
   }));
 });
