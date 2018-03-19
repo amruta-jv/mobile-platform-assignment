cf7567fc1b1367dd6953da4d7ecafb54e9e20435
Amruta Jegarkal <amruta@simpragma.com>
2018-03-19 17:46:02 +0530

    Adds tests for design screen component

--- mobile-platform-ui/src/app/design-screen/design-screen.component.spec.ts
+++ mobile-platform-ui/src/app/design-screen/design-screen.component.spec.ts
@@ -1,5 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing'
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 import { DesignScreenComponent } from './design-screen.component';
 import { AppService } from '../../shared/app.global';
@@ -26,4 +27,29 @@ describe('DesignScreenComponent', () => {
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have Widgets, Design, Properties label', async(() => {
+    const fixture = TestBed.createComponent(DesignScreenComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    let labels = compiled.querySelectorAll('label')
+    expect(labels[0].textContent).toContain('Widgets');
+    expect(labels[1].textContent).toContain('Design');
+    expect(labels[2].textContent).toContain('Properties');
+  }));
+
+  it('should Preview button', async(() => {
+    const fixture = TestBed.createComponent(DesignScreenComponent);
+    fixture.detectChanges();
+    let previewBtn = fixture.debugElement.query(By.css('.preview-btn'));
+    expect(previewBtn.nativeElement.textContent).toEqual('Preview');
+  }));
+
+  it('should have iPhone 5S mobile image', async(() => {
+    const fixture = TestBed.createComponent(DesignScreenComponent);
+    fixture.detectChanges();
+    let designBgImage = fixture.debugElement.query(By.css('.mobile-image'));
+    expect(designBgImage.nativeElement.src).toContain('iphone5S-portrait.png');
+  }));
+  
 });
