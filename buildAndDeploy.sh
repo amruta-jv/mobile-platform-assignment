81d54cc6eed92a235c869e4d1622a94433cba518
Amruta Jegarkal <amruta@simpragma.com>
2018-03-20 13:43:52 +0530

    Removes test steps

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -3,15 +3,5 @@
 cd mobile-platform-ui
 npm install
 node_modules/.bin/ng --version
-
-echo "Started running unit test cases"
-node_modules/.bin/ng test
-echo "Completed running unit test cases"
-
-echo "Started running e2e test cases"
-node_modules/.bin/ng serve &
-node_modules/.bin/ng e2e
-echo "Completed running e2e test cases"
-
 node_modules/.bin/ng build --prod
 ls
