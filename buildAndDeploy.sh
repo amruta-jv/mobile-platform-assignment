091f06bf8cbac6ab9ac9a664c3ae6bf568c5c367
Amruta Jegarkal <amruta@simpragma.com>
2018-03-20 17:00:23 +0530

    Adds ls -al command

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -5,8 +5,8 @@ npm install
 node_modules/.bin/ng --version
 node_modules/.bin/ng build --prod
 echo "-----------------------"
-ls
+ls -al
 echo "Generating tar file of deployable"
 tar -zcvf mobile-platform-ui.tar.gz dist
 echo "Generated tar file of deployable"
-ls
+ls -al
