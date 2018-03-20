5a8da4c48c87ce6095bb55f34a13b5c519e09c62
Amruta Jegarkal <amruta@simpragma.com>
2018-03-20 15:20:48 +0530

    Gives super user access to zip statement

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -4,5 +4,6 @@ cd mobile-platform-ui
 npm install
 node_modules/.bin/ng --version
 node_modules/.bin/ng build --prod
-apt-get install zip
-zip -r mobile-platform-ui.zip dist
+sudo apt-get install zip
+sudo zip -r mobile-platform-ui.zip dist
+ls
