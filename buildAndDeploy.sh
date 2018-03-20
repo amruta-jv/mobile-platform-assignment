12f02a96862844e698f775b0442541dbfb0f0c93
Amruta Jegarkal <amruta@simpragma.com>
2018-03-20 16:55:23 +0530

    Adds echo statements

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -4,6 +4,9 @@ cd mobile-platform-ui
 npm install
 node_modules/.bin/ng --version
 node_modules/.bin/ng build --prod
+echo "-----------------------"
+ls
+echo "Generating tar file of deployable"
 tar -zcvf mobile-platform-ui.tar.gz dist
-echo "---------"
+echo "Generated tar file of deployable"
 ls
