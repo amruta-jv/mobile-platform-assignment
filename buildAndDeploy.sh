c2b655cd886d9dec81f0ebd7d4cbc39c7859a62f
Amruta Jegarkal <amruta@simpragma.com>
2018-03-20 13:25:51 +0530

    Adds echo statement and statement to know angular version

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -2,5 +2,8 @@
 
 cd mobile-platform-ui
 npm install
+echo "------------------------------------"
+node_modules/.bin/ng --version
+echo "------------------------------------"
 node_modules/.bin/ng build --prod
 ng build --prod
