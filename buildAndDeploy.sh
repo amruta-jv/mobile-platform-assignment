53cf99a7982e877d52972b2b2f03bac0f1908a56
Amruta Jegarkal <amruta@simpragma.com>
2018-03-20 13:30:52 +0530

    Removes echo statements

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -2,8 +2,6 @@
 
 cd mobile-platform-ui
 npm install
-echo "------------------------------------"
 node_modules/.bin/ng --version
-echo "------------------------------------"
 node_modules/.bin/ng build --prod
 ng build --prod
