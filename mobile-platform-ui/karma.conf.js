58ad7ddd5a8840ebb1b42fc28ad4051e2c61c2a1
Amruta Jegarkal <amruta@simpragma.com>
2018-03-21 18:34:55 +0530

    Comments e2e steps

--- mobile-platform-ui/karma.conf.js
+++ mobile-platform-ui/karma.conf.js
@@ -28,11 +28,22 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['Chrome'],
     singleRun: true,
     phantomjsLauncher: {
       // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
       exitOnResourceError: true
+    },
+    customLaunchers: {
+      ChromeHeadless: {
+        base: 'Chrome',
+        flags: [
+          '--headless',
+          '--disable-gpu',
+      // Without a remote debugging port, Google Chrome exits immediately.
+          '--remote-debugging-port=9222'
+        ]
+      }
     }
   })
 }
