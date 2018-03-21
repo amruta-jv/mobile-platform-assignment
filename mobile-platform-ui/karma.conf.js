b3d9b5128f650b07ca891af19f0a40842534b475
Amruta Jegarkal <amruta@simpragma.com>
2018-03-21 13:58:37 +0530

    Updates config to make tests to run single time

--- mobile-platform-ui/karma.conf.js
+++ mobile-platform-ui/karma.conf.js
@@ -29,7 +29,7 @@ module.exports = function (config) {
     logLevel: config.LOG_DEBUG,
     autoWatch: true,
     browsers: ['PhantomJS'],
-    singleRun: false,
+    singleRun: true,
     phantomjsLauncher: {
       // Have phantomjs exit if a ResourceError is encountered (useful if karma exits without killing phantom)
       exitOnResourceError: true
