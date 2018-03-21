34d5cb37a1419d673e870398dbd18d87af8bbe75
Amruta Jegarkal <amruta@simpragma.com>
2018-03-21 14:26:43 +0530

    Updates steps for chrome installation

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -15,10 +15,10 @@ fi
 sudo apt-get update
 sudo apt-get install xvfb -y
 
-sudo sh -c ‘echo “deb [arch=amd64] http://dl-ssl.google.com/linux/chrome/deb/ stable main” >> /etc/apt/sources.list.d/google-chrome.list’
- wget -q -O —  https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo apt-get update 
-sudo apt-get install google-chrome-stable -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg --unpack google-chrome-stable_current_amd64.deb
+sudo apt-get install -f -y
+sudo apt-get clean
 
 npm install webdriver-manager
 ./node_modules/protractor/bin/webdriver-manager update
