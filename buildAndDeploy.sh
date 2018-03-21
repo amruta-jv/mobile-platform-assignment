84d7792cbba23261ad63db342f2fd85a7fd32420
Amruta Jegarkal <amruta@simpragma.com>
2018-03-21 14:18:31 +0530

    Adds configuration/environment needed for e2e

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -10,6 +10,24 @@ if [ $? -ne 0 ]; then
     exit 1;
 fi
 
+
+#Steps for e2e test cases
+sudo apt-get update
+sudo apt-get install xvfb -y
+
+sudo sh -c ‘echo “deb [arch=amd64] http://dl-ssl.google.com/linux/chrome/deb/ stable main” >> /etc/apt/sources.list.d/google-chrome.list’
+ wget -q -O —  https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update 
+sudo apt-get install google-chrome-stable -y
+
+npm install webdriver-manager
+./node_modules/protractor/bin/webdriver-manager update
+./node_modules/protractor/bin/webdriver-manager start > log.txt &
+
+# Xvfb :99 &
+# export DISPLAY=:99
+# sleep 20s
+
 node_modules/.bin/ng serve &
 node_modules/.bin/ng e2e
 if [ $? -ne 0 ]; then
