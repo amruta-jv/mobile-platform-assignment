db9524a299c41751ea9c6433949ef7f57dcf4ecc
Amruta Jegarkal <amruta@simpragma.com>
2018-03-21 12:10:27 +0530

    Adds proper alignment for preview page

--- mobile-platform-ui/src/app/preview/preview.component.ts
+++ mobile-platform-ui/src/app/preview/preview.component.ts
@@ -19,6 +19,12 @@ export class PreviewComponent implements OnInit {
   	let globalVal = this.appService.getAppGlobals();
     var div = document.getElementById('preview-div');
     div.innerHTML = globalVal.design != undefined ? globalVal.design.trim() : "";
+    for (var i = 0; i < div.childNodes.length; ++i) {
+      let currentDiv = document.getElementById(div.childNodes[i]['id']);
+      let currentLeft = currentDiv.style.left.replace("px", "");
+      let latestLeft = parseFloat(currentLeft) + 27; 
+      currentDiv.style.left = latestLeft + "px";
+    }
   }
 
 }
