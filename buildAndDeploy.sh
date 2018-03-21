89e6aeb96a0af8a4ff511e0635b9cf4cb5548408
Amruta Jegarkal <amruta@simpragma.com>
2018-03-21 14:05:56 +0530

    Adds steps for e2e tests cases run

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -9,6 +9,14 @@ if [ $? -ne 0 ]; then
     echo "ERROR: Unit tests failed."
     exit 1;
 fi
+
+node_modules/.bin/ng serve &
+node_modules/.bin/ng e2e
+if [ $? -ne 0 ]; then
+    echo "ERROR: End to end tests failed."
+    exit 1;
+fi
+
 # node_modules/.bin/ng build --prod
 # tar -zcvf mobile-platform-ui.tar.gz dist
 
