eff99825ec532c549680636fe4306c9d89b697e6
Amruta Jegarkal <amruta@simpragma.com>
2018-03-21 14:29:23 +0530

    Adds sudo to dpkg command

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -16,7 +16,7 @@ sudo apt-get update
 sudo apt-get install xvfb -y
 
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg --unpack google-chrome-stable_current_amd64.deb
+sudo dpkg --unpack google-chrome-stable_current_amd64.deb
 sudo apt-get install -f -y
 sudo apt-get clean
 
