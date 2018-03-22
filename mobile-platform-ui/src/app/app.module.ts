915efe392e5aedf7624d3df312f2338421c46e4a
Amruta Jegarkal <amruta@simpragma.com>
2018-03-22 10:53:46 +0530

    Adds documentation for functions and renames file

--- mobile-platform-ui/src/app/app.module.ts
+++ mobile-platform-ui/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { PreviewComponent } from './preview/preview.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DesignScreenComponent } from './design-screen/design-screen.component';
-import { AppService } from '../shared/app.global';
+import { AppService } from '../shared/app.service';
 
 const appRoutes: Routes = [
   { path: 'preview', component: PreviewComponent },
--- mobile-platform-ui/src/app/design-screen/design-screen.component.ts
+++ mobile-platform-ui/src/app/design-screen/design-screen.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { AppService } from '../../shared/app.global';
+import { AppService } from '../../shared/app.service';
 
 @Component({
   selector: 'app-design-screen',
@@ -9,14 +9,20 @@ import { AppService } from '../../shared/app.global';
 })
 export class DesignScreenComponent {
 
-  constructor(private router: Router, private route: ActivatedRoute, private appService: AppService) {
+	constructor(private router: Router, private route: ActivatedRoute, private appService: AppService) {
 
 	}
 
+	/**
+		'onDragStart' event handler function.
+	*/
 	onDragStart(event, selectedWidget) {
 		event.dataTransfer.setData('selectedWidget', selectedWidget);
 	}
 
+	/**
+		'onDrop' event handler function.
+	*/
 	onDrop(event) {
 		event.preventDefault();
 		let draggedWidget = event.dataTransfer.getData('selectedWidget');
@@ -24,21 +30,30 @@ export class DesignScreenComponent {
 		this.setPositionOfWidget(widget, event.offsetX , event.offsetY);
 	}
 
+
+	/**
+		'allowDrop' event handler function.
+	*/
 	allowDrop(event) {
 		event.preventDefault();
 	}
 
+	/**
+		This function creates selected widget and bundles it into a div and returns it.
+		@param {DOMElement} draggedWidget - Selected/dragged widget
+		@return {DOMElement} divWidget - Div element with widget inside it. 
+	*/
 	constructWidgetByName(draggedWidget) {
 		let widget;
 		switch (draggedWidget) {
-			case 'input': widget = document.createElement('input');
-						  widget['id'] = "input-"
-						  break;
-		    case 'button': widget = document.createElement('button');
-						  widget['id'] = "button-"
-						  widget.setAttribute('class', "btn-primary");
-						  widget.innerHTML = 'Submit';
-						  break;
+		case 'input': widget = document.createElement('input');
+					  widget['id'] = "input-"
+					  break;
+		case 'button': widget = document.createElement('button');
+					  widget['id'] = "button-"
+					  widget.setAttribute('class', "btn-primary");
+					  widget.innerHTML = 'Submit';
+					  break;
 		}
 		widget['id'] = widget['id'] + (document.getElementById("design-div").childElementCount + 1);
 		let divWidget = document.createElement('div');
@@ -47,6 +62,12 @@ export class DesignScreenComponent {
 		return divWidget;
 	}
 
+	/**
+		This function creates selected widget and bundles it into a div and returns it.
+		@param {DOMElement} widget - Constructed widget
+		@param {Number} leftPos - offsetX value from onDrop event
+		@param {Number} topPos - offsetY value from onDrop event
+	*/
 	setPositionOfWidget(widget, leftPos, topPos) {
 		document.getElementById("design-div").appendChild(widget);
 		let position = "position: absolute; left: "+ leftPos + "px;";
@@ -54,6 +75,10 @@ export class DesignScreenComponent {
 		document.getElementById(widget['id']).setAttribute("style", position);
 	}
 
+
+	/**
+		This function saves designed page to gloabl variable and redirects to preview component.
+	*/
 	showPreview() {
 		this.appService.updateAppGlobals("design", document.getElementById('design-div').innerHTML);
 		this.router.navigate(["/preview"]);
