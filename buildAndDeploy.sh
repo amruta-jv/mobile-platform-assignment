129aca6e375015232db41fda8e749d8ce00c0a59
Amruta Jegarkal <amruta@simpragma.com>
2018-03-22 16:15:16 +0530

    Adds steps for generating deployable

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -1,26 +1,35 @@
 #!/bin/sh
 
 cd mobile-platform-ui
+
+# Installs node modules
 npm install
-node_modules/.bin/ng --version
 
-node_modules/.bin/ng serve &
-
-echo "-----------------------------"
-node_modules/.bin/ng e2e
-if [ $? -ne 0 ]; then
-    echo "ERROR: e2e tests failed."
-    exit 1;
-fi
-
+# Unit tests
 node_modules/.bin/ng test
 if [ $? -ne 0 ]; then
     echo "ERROR: Unit tests failed."
     exit 1;
 fi
 
-# node_modules/.bin/ng build --prod
-# sudo cp -r dist/*  /srv/www/gui-builder/ 
+
+# e2e test
+node_modules/.bin/ng serve &
+node_modules/.bin/ng e2e
+if [ $? -ne 0 ]; then
+    echo "ERROR: e2e tests failed."
+    exit 1;
+fi
+
+
+# Generates 'dist' deployable
+node_modules/.bin/ng build --prod
+
+# Creates a zip file
+sudo zip -r mobile-platform-ui.zip dist
+
+# Deploys to apache2 server
+sudo cp -r dist/*  /srv/www/gui-builder/
 
 
 
