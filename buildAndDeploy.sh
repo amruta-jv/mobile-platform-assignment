1e7183b07b8b1e606f1f0802b860d8e21ebb2518
Amruta Jegarkal <amruta@simpragma.com>
2018-03-22 15:27:22 +0530

    Adds e2e configuration for headless mode

--- buildAndDeploy.sh
+++ buildAndDeploy.sh
@@ -4,14 +4,23 @@ cd mobile-platform-ui
 npm install
 node_modules/.bin/ng --version
 
-node_modules/.bin/ng test
+node_modules/.bin/ng serve &
+
+echo "-----------------------------"
+node_modules/.bin/ng e2e
 if [ $? -ne 0 ]; then
-    echo "ERROR: Unit tests failed."
+    echo "ERROR: e2e tests failed."
     exit 1;
 fi
 
-node_modules/.bin/ng build --prod
-sudo cp -r dist/*  /srv/www/gui-builder/ 
+# node_modules/.bin/ng test
+# if [ $? -ne 0 ]; then
+#     echo "ERROR: Unit tests failed."
+#     exit 1;
+# fi
+
+# node_modules/.bin/ng build --prod
+# sudo cp -r dist/*  /srv/www/gui-builder/ 
 
 
 
