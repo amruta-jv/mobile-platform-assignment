737c1d12486ff39a79200a8193b230514ad7538a
Amruta Jegarkal <amruta@simpragma.com>
2018-03-22 16:16:38 +0530

    Adds version to header and updates test

--- mobile-platform-ui/src/app/app.component.ts
+++ mobile-platform-ui/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
+const { version: appVersion } = require('../../package.json')
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +10,11 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class AppComponent {
 	title = 'Mahindra Comviva Mobile Platform UI';
+	public appVersion
 
+	 constructor() {
+	 	this.appVersion = appVersion
+	 }
 
 }
 
