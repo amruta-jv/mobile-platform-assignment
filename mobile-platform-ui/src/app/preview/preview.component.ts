8766e257d0eefda00229cbfb8e712cee7d0770e4
Amruta Vijayakumar Jegarkal <amruta.jegarkal@COMVIVA.COM>
2018-04-09 17:26:45 +0530

    Adds generateJason() method

--- mobile-platform-ui/src/app/preview/preview.component.ts
+++ mobile-platform-ui/src/app/preview/preview.component.ts
@@ -28,6 +28,42 @@ export class PreviewComponent implements OnInit {
       let latestLeft = parseFloat(currentLeft) + 27; 
       currentDiv.style.left = latestLeft + "px";
     }
+    this.generateJason();
+  }
+
+  generateJason() {
+    let globalVal = this.appService.getAppGlobals();
+    console.log(document.getElementById('preview-div').childNodes)
+    let previewDiv = document.getElementById('preview-div');
+        let jason: any = {"$jason": {
+                                        "head": {}, 
+                                        "body": {"sections": [ 
+                                                          {"items": []}
+                                                  ]
+                                                }
+                                    }
+                          };
+    for (var i = 0; i < previewDiv.childNodes.length; ++i) {
+      console.log("CHILD NODE" + previewDiv.childNodes[i].childNodes[0]);
+      console.log(jason["$jason"]);
+      let item = this.getItemJasonByWidget(previewDiv.childNodes[i].childNodes[0]);
+      jason["$jason"].body.sections[0].items.push(item);
+      console.log( jason["$jason"]);
+    }
+
+  }
+
+
+  getItemJasonByWidget(widget) {
+     let item; 
+     switch(widget.localName) {
+        case 'input': item = {"type": "textfield"};
+                      break;
+        case 'button': item = {"type": "button"};
+                       break;
+     }
+
+     return item;
   }
 
 }
